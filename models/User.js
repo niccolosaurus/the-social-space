43f1c235361776808040d3ae0ee3fdb45f1dbf10
Niccolo Eck <nicco.eck@gmail.com>
2022-04-18 20:47:47 -0700

    added my User model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+const validator = require('validator');
+
+//User Model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: () => Promise.resolve(false),
+                message: 'Email validation failed'
+            },
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJson: {
+            getters: true,
+            virtuals: true,
+        },
+    }
+);
+
+//friendCount Virtual
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
