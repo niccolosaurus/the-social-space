7448e6096c763675cd9d805cc3bee8057149d799
Niccolo Eck <nicco.eck@gmail.com>
2022-04-30 09:24:29 -0700

    updated my thoughtController with better routes and code.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,41 +3,52 @@ const { User, Thought } = require('../models');
 module.exports = {
   getThoughts(req, res) {
     Thought.find()
-      .then(async (thoughts) => {
-        const thoughtObject = {
-          thoughts,
-        };
-        return res.json(thoughtObject);
-      })
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
-      });
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+      // .then(async (thoughts) => {
+      //   const thoughtObject = {
+      //     thoughts,
+      //   };
+      //   return res.json(thoughtObject);
+      // })
+      // .catch((err) => {
+      //   console.log(err);
+      //   return res.status(500).json(err);
+      // });
   },
 
   getSingleThought(req, res) {
-    Thought.findById({ _id: req.params.thoughtId })
+    // Thought.findById({ _id: req.params.thoughtId })
+    Thought.findOne({_id: req.params.thoughtId})
       .select('-__v')
-      .then(async (thought) =>
+      .then((thought) =>
         !thought
           ? res
               .status(404)
               .json({ message: 'Nothing with that ID' })
-          : res.json({
-              thought,
-            }),
+          : res.json({thought})
       )
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
-      });
+      .catch((err) => res.status(500).json(err));
   },
 
   createThought(req, res) {
     Thought.create(req.body)
-      .then((thought) => res.json(thought))
-      .catch((err) => res.status(500).json(err));
+      User.findOneAndUpdate(
+        {_id: req.params.userID},
+        {$addToSet: {thoughts: req.body}},
+        {runValidators: true, new: true}
+      )
+        .then((thought) =>
+          !thought
+            ?res.status(404)
+            .json({message: 'No user with that ID'})
+          :res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+      // .then((thought) => res.json(thought))
+      // .catch((err) => res.status(500).json(err));
   },
+
 
   deleteThought(req, res) {
     Thought.findOneAndRemove({ _id: req.params.thoughtId })
@@ -54,15 +65,10 @@ module.exports = {
       )
       .then((reaction) =>
         !reaction
-          ? res.status(404).json({
-              message: 'User deleted',
-            })
+          ? res.status(404).json({message: 'User deleted'})
           : res.json({ message: 'Thought deleted' }),
       )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+      .catch((err) => res.status(500).json(err));
   },
 
   updateThought(req, res) {
@@ -102,7 +108,7 @@ module.exports = {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
       { $pull: { reactions: { _id: req.params.reactionId } } },
-      { new: true },
+      { runValidators: true, new: true },
     )
       .then((thought) =>
         !thought
