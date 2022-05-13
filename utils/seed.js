b17cbb45d7444a4616a99579a209dd1adfc54a46
Niccolo Eck <nicco.eck@gmail.com>
2022-05-13 14:13:16 -0700

    removed the seeded data

--- utils/seed.js
+++ utils/seed.js
@@ -1,37 +1,37 @@
-const connection = require('../config/connection');
-const { User, Thought } = require('../models');
-const { getRandomName } = require('./data');
+// const connection = require('../config/connection');
+// const { User, Thought } = require('../models');
+// const { getRandomName } = require('./data');
 
-connection.on('error', (err) => err);
+// connection.on('error', (err) => err);
 
-connection.once('open', async () => {
-    console.log('connected');
+// connection.once('open', async () => {
+//     console.log('connected');
 
-    //Drop existing users
-    await User.deleteMany({});
+//     //Drop existing users
+//     await User.deleteMany({});
 
-    //Drop existing thoughts
-    await Thought.deleteMany({});
+//     //Drop existing thoughts
+//     await Thought.deleteMany({});
 
-    //Array that holds the users
-    const users = [];
+//     //Array that holds the users
+//     const users = [];
 
-    //Loops 20 times -- gets us usernames and emails
-    for (let i =0; i < 20; i++) {
-        const username = getRandomName();
-        const email = `${username}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}@email.com`;
+//     //Loops 20 times -- gets us usernames and emails
+//     for (let i =0; i < 20; i++) {
+//         const username = getRandomName();
+//         const email = `${username}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}@email.com`;
 
-        users.push({
-            username,
-            email,
-        });
-    }
+//         users.push({
+//             username,
+//             email,
+//         });
+//     }
 
-    //Add users to the collection and await the results
-    await User.collection.insertMany(users);
+//     //Add users to the collection and await the results
+//     await User.collection.insertMany(users);
 
-    //Logout the seed data to indicate what should appear in the database
-    console.table(users);
-    console.info('Seeding complete! 🌱');
-    process.exit(0);
-});
+//     //Logout the seed data to indicate what should appear in the database
+//     console.table(users);
+//     console.info('Seeding complete! 🌱');
+//     process.exit(0);
+// });
